045702690384bb5547f109fc51e724944a6f574f
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-09-24 23:02:15 +0200

    Update .env.example for postgres, prepare for bot

--- env-gen.sh
+++ env-gen.sh
@@ -3,3 +3,4 @@
 # Example .env generator
 sed 's/=.*/=/' ./src/.env.production > ./src/.env.production.example
 sed 's/=.*/=/' ./src/.env.development > ./src/.env.development.example
+sed 's/=.*/=/' ./postgres/.env > ./postgres/.env.example
