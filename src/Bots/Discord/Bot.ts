cc6552ed50a7572bc52c6487dfcad4e80153aa2a
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-09-26 18:51:54 +0200

    Wrap bot.ts into a class

--- src/Bots/Discord/Bot.ts
+++ src/Bots/Discord/Bot.ts
@@ -2,20 +2,22 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { Environment } from '@Bootstrap/Environment';
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ],
-});
-
-export async function run() {
-    client.once(Events.ClientReady, (readyClient) => {
-        console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+export class Bot {
+    private static readonly client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers,
+        ],
     });
     
-    console.log(`Starting Discord Bot...`);
-    await client.login(Environment.env.DISCORD_BOT_TOKEN);
+    static async run(): Promise<void> {
+        Bot.client.once(Events.ClientReady, (readyClient) => {
+            console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+        });
+        
+        console.log(`Starting Discord Bot...`);
+        await Bot.client.login(Environment.env.DISCORD_BOT_TOKEN);
+    }
 }
